ca00db04254e8aeb341dfd922548e53b476f1eb1
lkhzth <lkhzth@gmail.com>
2023-04-06 17:59:26 +0900

    최신수정

--- WebContent/resources/sql/장바구니.sql
+++ WebContent/resources/sql/장바구니.sql
@@ -1,22 +1,40 @@
 drop TABLE cart_tbl;
+DROP SEQUENCE cartNum_SEQ;
 
 CREATE TABLE cart_tbl (
-  cartId NUMBER PRIMARY KEY,
-  memberId VARCHAR2(50) NOT NULL,
+  cartNum NUMBER PRIMARY KEY,
+  mno NUMBER NOT NULL,
   product_bno NUMBER NOT NULL,
-  product_Count number,
-  FOREIGN KEY (memberId) REFERENCES member_tbl (memberId),
+  product_Count NUMBER,
+  FOREIGN KEY (mno) REFERENCES member_tbl (mno),
   FOREIGN KEY (product_bno) REFERENCES PRODUCT_TBL (PRODUCT_BNO)
 );
 
--- 유니크 제약조건
-alter table cart_tbl add unique (memberId, product_bno);
--- cartId not null 제약조건
-ALTER TABLE cart_tbl MODIFY (cartId NUMBER NOT NULL);
+CREATE SEQUENCE cartNum_SEQ;
 
-select * from cart_tbl;
+INSERT INTO cart_tbl(cartNum, mno, product_bno, product_Count)
+VALUES (cartNum_SEQ.NEXTVAL, 2, 1, 1);
+
+--  회원번호, 상품번호 
+select sum(product_count) as product_count  from cart_tbl where mno=2 and product_bno=1; 
+
 
 COMMIT;
+
+
+
+
+SELECT 
+		  c.cartNum, c.mno, c.product_Count,
+		  p.PRODUCT_BNO, p.PRODUCT_NAME, p.PRODUCT_PRICE
+		FROM 
+		  cart_tbl c
+		JOIN member_tbl m ON c.mno = m.mno
+		JOIN product_tbl p ON c.product_bno = p.PRODUCT_BNO
+		where c.mno=2;
+
+
+
 
 
 
